858e3c4a8c7900583994277fffd1ac49ecd5d400
neighborhoodViet <anhoang2832005@gmail.com>
2025-10-13 21:07:48 -0400

    Added "skip event" capabilities

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands, tasks
 import logging
 from dotenv import load_dotenv
 import os
-
+import json
 from random import randint
 from math import ceil, floor
 from datetime import datetime
@@ -13,7 +13,6 @@ from gemini import *
 from calendarAPI import *
 
 load_dotenv()
-
 token = os.getenv('DISCORD_TOKEN')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -21,10 +20,8 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-
-valid67gifs = ["https://tenor.com/knFBPBaMB4Z.gif", "https://tenor.com/q0WNC9BA9Cr.gif", "https://tenor.com/glbfYP7tYWr.gif", "https://tenor.com/rdtFCkDpIOF.gif", "https://tenor.com/fG1WQ87kpnp.gif"]
-
 bot = commands.Bot(command_prefix='!', intents=intents) #! hello"
+
 
 @bot.event
 async def on_ready():
@@ -33,38 +30,45 @@ async def on_ready():
     global birthday_channel
     global bot_guild
 
-    repost_channel = bot.get_channel(1419688863475040288)
-    birthday_channel = bot.get_channel(1355030340607017102)
-    bot_guild = bot.get_guild(1336139315079548999)
+    with open('guild_channels.json', 'r') as f:
+        guild_channels = json.load(f)
+
+    repost_channel = bot.get_channel(guild_channels["repost_channel"])
+    birthday_channel = bot.get_channel(guild_channels['birthday_channel'])
+    bot_guild = bot.get_guild(guild_channels['bot_guild'])
 
     main_loop.start()
+
+
+valid67gifs = ["https://tenor.com/knFBPBaMB4Z.gif", "https://tenor.com/q0WNC9BA9Cr.gif", "https://tenor.com/glbfYP7tYWr.gif", "https://tenor.com/rdtFCkDpIOF.gif", "https://tenor.com/fG1WQ87kpnp.gif"]
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
     
-    elif ("67" in message.content or "6 7" in message.content):
+    elif ("67" in message.content or "6 7" in message.content) and not message.mentions:
         await message.reply(valid67gifs[randint(0, len(valid67gifs) - 1)])
 
-    elif message.author.name == "beomgyu._.":
-        if randint(0, 20) == 0 or bot.user in message.mentions:
-            await message.reply(f"Yes my king {message.author.mention}")
-
-    author_roles = [x.name for x in message.author.roles]
+    elif message.author.name == "beomgyu._." and bot.user in message.mentions:
+        await message.reply(f"Yes my king {message.author.mention}")
 
     await bot.process_commands(message)
 
 
-asa_members = {"Jaira": 1405950487622324284, "David": 461679009131003937, "Sakina": 1291219047731433482, "Thien": 477488906489561099, "Cathy": 816122867921453076, "Antinet": 654086056924151808, "An": 531296756613382165, "Uyanga": 755435417993740309, "Clare": 1358923944983531642, "Divya": 1288632175108947968}
+with open('users.json', 'r') as u:
+    asa_members = json.load(u)
 
 temp = []
 last_day_updated = None
 time_to_post = None
+skip = True
+skipped_event = None
 
 @tasks.loop(hours=1)
 async def main_loop():
     next_event = get_next_event()
+    global next_event_name
     next_event_name = next_event[0]
     days_remaining = next_event[1]
 
@@ -75,9 +79,17 @@ async def main_loop():
     global temp
     global last_day_updated
     global time_to_post
+    global skip
+    global skipped_event
 
     if not temp:
         temp = list(asa_members.values())
+
+    if skip:
+        if skipped_event != next_event_name:
+            skip = not skip
+            return
+        return
 
     now = datetime.now(tz=timezone("US/Eastern"))
     if (last_day_updated != now.day and now.hour == time_to_post) or not last_day_updated or not time_to_post:
@@ -102,7 +114,25 @@ async def main_loop():
             
             result_str += f"{user_to_mention} "
 
-        await repost_channel.send(f"{result_str}\n Please repost {next_event_name}\n {10 - len(selected_reposters)} / 10 remaining")
+        await repost_channel.send(f"{result_str}\n Please repost **{next_event_name}**.")
+
+@bot.command()
+async def skip_event(ctx):
+    global skip
+    global skipped_event
+    skip = not skip
+    skipped_event = next_event_name
+
+    await ctx.send(f"Repost pings have been set to {skip}")
+
+@bot.command()
+async def unskip_event(ctx):
+    global skip
+    global skipped_event
+    skip = not skip
+    skipped_event = next_event_name
+
+    await ctx.send(f"Repost pings have been set to {skip}")
 
 if __name__ == '__main__':
     bot.run(token, log_handler=handler, log_level=logging.DEBUG)
